55b62b090ba3540f467559a1d743bed75c30c8f8
Jonathan Tyler Weber <jonathantweber@gmail.com>
2025-12-19 13:09:37 -0600

    alphabetCipher ts encode

---
+++ java/app/src/main/java/AlphabetCipher.java
@@ -0,0 +1,21 @@
+public class AlphabetCipher {
+
+    /**
+     * Alphabet Cipher (Vigenère-style)
+     *
+     * Rules for this kata starter:
+     * - Treat letters as a-z only
+     * - Normalize by lowercasing
+     * - Ignore non [a-z] characters in BOTH message and keyword
+     * - Output is lowercase a-z only
+     */
+    public static String encode(String message, String keyword) {
+        // TODO: implement
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    public static String decode(String ciphertext, String keyword) {
+        // TODO: implement
+        throw new UnsupportedOperationException("Not implemented");
+    }
+}
